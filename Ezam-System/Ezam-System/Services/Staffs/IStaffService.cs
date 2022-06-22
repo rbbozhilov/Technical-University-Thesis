1a1e00e7d8ab97698f6db6b8206000f6ea8de310
rbbozhilov <rbbojilov@gmail.com>
2022-06-22 22:46:49 +0300

    Create service for staff and adding create,edit,delete methods

---
+++ Ezam-System/Ezam-System/Services/Staffs/IStaffService.cs
@@ -0,0 +1,28 @@
+﻿namespace Ezam_System.Services.Staffs
+{
+    public interface IStaffService
+    {
+
+        void Create(
+                  string fullname,
+                  string position,
+                  string email,
+                  string phoneNumber,
+                  string imageUrl,
+                  string office);
+
+
+        bool Edit(
+                  int id,
+                  string fullname,
+                  string position,
+                  string email,
+                  string phoneNumber,
+                  string imageUrl,
+                  string office);
+
+        bool Delete(int id);
+
+
+    }
+}
---
+++ Ezam-System/Ezam-System/Services/Staffs/StaffService.cs
@@ -0,0 +1,91 @@
+﻿using Ezam_System.Data;
+using Ezam_System.Data.Models;
+using System.Linq;
+
+namespace Ezam_System.Services.Staffs
+{
+    public class StaffService : IStaffService
+    {
+
+        private EzamDbContext data;
+
+        public StaffService(EzamDbContext data)
+        {
+            this.data = data;
+        }
+
+
+        public void Create(
+                           string fullname,
+                           string position,
+                           string email,
+                           string phoneNumber,
+                           string imageUrl,
+                           string office)
+        {
+
+            var newStaff = new Staff()
+            {
+                FullName = fullname,
+                Position = position,
+                Email = email,
+                PhoneNumber = phoneNumber,
+                ImageUrl = imageUrl,
+                Office = office
+            };
+
+            this.data.Staff.Add(newStaff);
+
+            this.data.SaveChanges();
+
+        }
+
+        public bool Delete(int id)
+        {
+            var currentStaff = this.data.Staff
+                                        .Where(s => s.Id == id && s.IsDeleted == false)
+                                        .FirstOrDefault();
+
+            if(currentStaff == null)
+            {
+                return false;
+            }
+
+
+            currentStaff.IsDeleted = true;
+
+            this.data.SaveChanges();
+
+            return true;
+        }
+
+        public bool Edit(
+                         int id, 
+                         string fullname,
+                         string position,
+                         string email,
+                         string phoneNumber,
+                         string imageUrl,
+                         string office)
+        {
+            var currentStaff = this.data.Staff
+                                        .Where(s => s.Id == id && s.IsDeleted == false)
+                                        .FirstOrDefault();
+
+
+            if(currentStaff == null)
+            {
+                return false;
+            }
+
+            currentStaff.FullName = fullname;
+            currentStaff.Position = position;
+            currentStaff.Email = email;
+            currentStaff.PhoneNumber = phoneNumber;
+            currentStaff.ImageUrl = imageUrl;
+            currentStaff.Office = office;
+
+            return true;
+        }
+    }
+}
